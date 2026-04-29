34ed71cdbefd8d6e073792537fb975b5b55d9b7d
yangcode <yanh814@foxmail.com>
2026-04-29 12:32:23 +0800

    hawp排除

--- hawp/base/show/__init__.py
+++
@@ -1,3 +0,0 @@
-from .canvas import Canvas, image_canvas, canvas
-from .painters import HAWPainter
-from .cli import cli, configure
--- hawp/ssl/models/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .detector import HAWP
-from . import detector
-from . import detector_with_heatmap
-from . import detector_with_hrheat
-from .registry import MODELS
